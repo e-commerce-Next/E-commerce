e3fe1e57898ee82e02aa679770bced63b58944bc
Shayma Dridi <dshayama@gmail.com>
2024-02-23 17:34:37 +0100

    userprofil+slleraddupdatedelete

--- nextjs-blog/app/page.tsx
+++ nextjs-blog/app/page.tsx
@@ -2,13 +2,15 @@ import About from "./AboutUs/page"
 import Nav from "./Navbar/page"
 import AllProducts from "./categorypage/page"
 import Footer from "./footer/page"
-import UpdateUserProfil from "./User/page"
+import SellerProfil from "./Seller/page"
+import SellerProductEdit from "./SellerProductEdit/page"
 
 export default function Home () {
 return (
     <div>
         <Nav/>
         {/* <AllProducts/> */}
+        <SellerProfil/>
         
         <Footer/>
         
